22e768f7767ff00dcd3f84365cb7d8ed21bbdcfb
Tetiana-Breslavska <tvk.koba@gmail.com>
2024-03-05 13:25:59 +0200

    Added totalCount to cart_icon

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,32 @@
+import {useState, useEffect} from 'react';
 import { Link } from "react-router-dom";
 import { useUser } from '../../context/use-user';
 import styles from './Navbar.module.scss';
 import { LocalStorageService } from "../../services/localStorage";
 
 export default function Navbar() {
+    const [ totalCount, setTotalCount ] = useState(0);
     const { user, setUser } = useUser();
-    // console.log(user);
+    const { addedBooks } = useUser();
+    console.log(addedBooks);
+
+    useEffect(()=>{
+        setTotalCount(addedBooks.reduce((acc, curr) => {return acc + curr.count}, 0));
+        
+    }, [addedBooks])
 
     const clean = () => {
         LocalStorageService.removeAll();
         setUser('');
-
     };
+
+
     return (
         <nav className={styles.navbar}>
             <div className='container d-flex justify-content-end'>
                 <Link className={styles.cart_icon} to="/cart">
                     <i className="fa-solid fa-cart-shopping"></i>
-                    <span className = {styles.cart_counter} >10</span>
+                    <span className={styles.cart_counter} >{totalCount}</span>
                 </Link>
                 <p>
                     <i className="fa-solid fa-user-large"></i>
